a576dc2c43da8360fb9835f1d0c054dd6ac70ff0
MrsMitrofeich <78724099+MrsMitrofeich@users.noreply.github.com>
2025-05-17 22:07:25 +0200

    refactor

---
+++ module/scripts/settings.js
@@ -0,0 +1,10 @@
+Hooks.once("init", () => {
+  game.settings.register("draw-sphere", "wsUrl", {
+    name: "WebSocket Server URL",
+    hint: "Адрес WebSocket-сервера, например ws://raspberrypi.local:8765",
+    scope: "world",
+    config: true,
+    type: String,
+    default: "ws://localhost:8765"
+  });
+});
